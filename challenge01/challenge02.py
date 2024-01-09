def4aafd9b6d86cfe139a66d81fca3e6932438b5
SSPENELOPE <typoepping@gmail.com>
2024-01-09 22:32:27 +0000

    Added Strings.

---
+++ challenge01/challenge02.py
@@ -0,0 +1,8 @@
+def main(): 
+    user_input = input("What is your name:")
+
+    user_input2 = input("What day of the week is it:");
+
+    print(f"Hello { user_input}, Happy {user_input2}!");
+
+main();
