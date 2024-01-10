c21df6c35d187ba4745157d2ef0bca542d3abaf3
SSPENELOPE <typoepping@gmail.com>
2024-01-10 16:20:11 +0000

    Added morning challenge.

---
+++ my_projects/challenge.py
@@ -0,0 +1,9 @@
+def main():
+    name = input("What is your name: ")
+    age = input("What is your age: ")
+    movie = input("What is your favorite movie: ")
+
+    print(f"Hello, {name.upper()}, you are {age} years old and your favorite movie is {movie}")
+if __name__ == "__main__":
+    main()
+
