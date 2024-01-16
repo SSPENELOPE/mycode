7afce563ab0e76038b22c5b9ea2343b3333bd3fa
SSPENELOPE <typoepping@gmail.com>
2024-01-16 16:07:24 +0000

    Added farm loop

---
+++ Labs/loopingwithfor.py
@@ -0,0 +1,17 @@
+def main():
+    farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+    ne_animals = farms[0]["agriculture"]
+    
+    for farm in farms:
+        print(f"{farm['name']}\n")
+    choice = input("Choose a farm");
+    
+    for farm in farms: 
+        if farm['name'].lower() == choice.lower():
+            print(f"{farm['agriculture']}")
+
+if __name__ == "__main__":
+    main()
